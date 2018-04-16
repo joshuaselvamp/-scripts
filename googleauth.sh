b281c2bb793a9348a7a36655ddcb525185785540
joshuaselvamp <38342595+joshuaselvamp@users.noreply.github.com>
2018-04-16 13:02:01 +0530

    Update googleauth.sh

--- googleauth.sh
+++ googleauth.sh
@@ -4,3 +4,10 @@ for i in ${user_list[@]}
 do
         find /home/$i/.google_authenticator -exec rsync {} /home/selvamj/2fa-ga/google_authenticator_$i  \; > /dev/null 2>&1
 done
+
+GIT=`which git`
+REPO_DIR=/home/selvamj/2fa-ga/
+cd ${REPO_DIR}
+${GIT} add --all  /home/selvamj/2fa-ga/
+${GIT} commit -m "google_authenticator_$i"
+${GIT} push git@stash.agilysys.local:7999/devop/2fa-googleauth.git
